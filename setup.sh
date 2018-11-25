faae0bf89b733488e9491b688835fe2900a881f9
fghtclb45 <onar1247@gmail.com>
2018-11-25 17:28:34 +0300

    Version 0.1

---
+++ setup.sh
@@ -0,0 +1,12 @@
+pkg update 
+pkg upgrade
+pkg install python
+pkg install python2 
+pip install --upgrade pip
+pkg install figlet
+pkg install nmap
+pip install pyfiglet
+pip install nmap 
+pkg install git
+pkg install wget 
+
